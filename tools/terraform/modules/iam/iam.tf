c72bd03b1b43f879461f9a928a9e8238ed08bcb9
Amo Wu <amo260@gmail.com>
2016-03-02 18:23:32 +0800

    feat(aws): add DynamoDB execution policy for Lambda function

--- tools/terraform/modules/iam/iam.tf
+++ tools/terraform/modules/iam/iam.tf
@@ -34,3 +34,30 @@ resource "aws_iam_role_policy" "cloudwatchlogs_full_access" {
 }
 EOF
 }
+
+resource "aws_iam_role_policy" "dynamodb_execution" {
+  name = "dynamodb_execution"
+  role = "${aws_iam_role.lambda_function.id}"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "dynamodb:DeleteItem",
+        "dynamodb:GetItem",
+        "dynamodb:PutItem",
+        "dynamodb:Query",
+        "dynamodb:Scan",
+        "dynamodb:UpdateItem",
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
