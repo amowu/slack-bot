1ca1ac397d750b0b3bd179561a4b548f3ac6ba9a
Amo Wu <amo260@gmail.com>
2016-04-14 16:32:33 +0800

    refactor(line): use ES2015 module pattern

--- functions/line/index.js
+++ functions/line/index.js
@@ -5,20 +5,23 @@ import 'babel-polyfill'
 /** Promise based XMLHttpRequests client */
 import axios from 'axios'
 
+import { getDialogue } from '../../lib/docomo'
+import translate from '../../lib/translate'
+
 export default async function (event, context) {
   console.log('Received event:', JSON.stringify(event, null, 2))
   try {
     // 1. 取得來自 LINE Channel 的 JSON Body.
     const msg = event.result[0]
     // 2. 將使用者輸入的文字翻譯成日本語
-    const source = await translate('ja', msg.content.text)
+    const source = await translate(process.env.GOOGLE_API_KEY, 'ja', msg.content.text)
     // 3. 使用 docomo 提供的聊天 API 和機器人對話
-    const dialogue = await getDialogue({
+    const dialogue = await getDialogue(process.env.DOCOMO_API_KEY, {
       utt: source.data.translations[0].translatedText,
       mode: 'dialog'
     })
     // 4. 將機器人回答的文字翻譯成使用者的語言
-    const target = await translate(source.data.translations[0].detectedSourceLanguage, dialogue.utt)
+    const target = await translate(process.env.GOOGLE_API_KEY, source.data.translations[0].detectedSourceLanguage, dialogue.utt)
     // 5. 將對話結果發送回 LINE Channel
     await sendMessage({
       to: [msg.content.from.toString()],
@@ -34,86 +37,6 @@ export default async function (event, context) {
   } catch (error) {
     context.fail(error.message)
   }
-}
-
-/**
- * Google Translate API
- * @see {@link https://cloud.google.com/translate/docs}
- * @param {string} target - 目標的語系，例：'en'。
- * @param {string} q - The text to be translated.
- * @return {Promise.<TranslateResults, Error>}
- * @typedef {Object[]} TranslateResults
- * @property {string} translatedText - 系統回答的文字，例：你好。
- * @property {string} detectedSourceLanguage - 偵測的來源語系。例：'zh-TW'。
- */
-function translate (target, q) {
-  return new Promise((resolve, reject) => {
-    axios({
-      method: 'GET',
-      baseURL: 'https://www.googleapis.com/',
-      url: '/language/translate/v2',
-      params: {
-        'key': process.env.GOOGLE_API_KEY,
-        'target': target,
-        'q': q
-      }
-    }).then(response => {
-      console.log('translate:', JSON.stringify(response, null, 2))
-      resolve(response.data)
-    }).catch(response => {
-      console.error('translate ERROR:', JSON.stringify(response, null, 2))
-      reject(new Error(response.data.statusMessage))
-    })
-  })
-}
-
-/**
- * docomo 雑談対話 API
- * @see {@link https://dev.smt.docomo.ne.jp}
- * @param {Object} data - docomo 雑談対話 API Request Body
- * @param {string} data.utt - 對話文字（255字以下），例：你好。
- * @param {string} data.context - 延續上一筆對話的 context ID。
- * @param {string} data.nickname - 使用者暱稱（10字以下），例：咲良。
- * @param {string} data.nickname_y - 暱稱的片假名讀音，例：さくら。
- * @param {string} data.sex - 使用者的性別（男 or 女）。
- * @param {string} data.bloodtype - 使用者的血型（A、B、AB、O）。
- * @param {string} data.birthdateY - 使用者的生日（年），例：1998。
- * @param {string} data.birthdateM - 使用者的生日（月），例：3。
- * @param {string} data.birthdateD - 使用者的生日（日），例：19。
- * @param {string} data.age - 使用者的年齡，例：16。
- * @param {string} data.constellations - 使用者的星座，例：魚座。
- * @param {string} data.place - 使用者所在的地區，例：鹿児島。
- * @param {string} data.mode - 對話模式，預設 dialog，延續對話 srtr。
- * @param {strung} data.t - 對話角色（20：關西腔角色、30：嬰兒角色）。
- * @return {Promise.<Dialogue, Error>}
- * @typedef {Object} Dialogue
- * @property {string} utt - 系統回答的文字，例：你好。
- * @property {string} yomi - 對話的片假名發音。例：こんにちは。
- * @property {string} mode - 對話模式（dialog or srtr），預設 dialog，延續對話 srtr。
- * @property {string} da - 對話編號，例：0。
- * @property {string} context - 用作延續下一筆對話的 context ID。
- */
-function getDialogue (data) {
-  return new Promise((resolve, reject) => {
-    axios({
-      method: 'POST',
-      baseURL: 'https://api.apigw.smt.docomo.ne.jp/dialogue/v1/',
-      url: '/dialogue',
-      headers: {
-        'Content-Type': 'application/json; charset=UTF-8'
-      },
-      params: {
-        'APIKEY': process.env.DOCOMO_API_KEY
-      },
-      data
-    }).then(response => {
-      console.log('getDialogue:', JSON.stringify(response, null, 2))
-      resolve(response.data)
-    }).catch(response => {
-      console.error('getDialogue ERROR:', JSON.stringify(response, null, 2))
-      reject(new Error(response.data.statusMessage))
-    })
-  })
 }
 
 /**
